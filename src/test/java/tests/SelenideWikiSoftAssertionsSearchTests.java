86298241c4aa6a9b225032357ae050a625cba294
Igor Belianchikov <belyanchikovia@gmail.com>
2024-10-25 23:15:31 +0200

    Changed test to find a JUnit5 example in the Selenide Wiki on GitHub to find exact text.

--- src/test/java/tests/SelenideWikiSoftAssertionsSearchTests.java
+++ src/test/java/tests/SelenideWikiSoftAssertionsSearchTests.java
@@ -3,16 +3,30 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideWikiSoftAssertionsSearchTests {
     @Test
     void selenideWikiSoftAssertionsShouldContainJUnit5Example() {
+        String expectedText = """
+                @ExtendWith({SoftAssertsExtension.class})
+                 class Tests {
+                   @Test
+                   void test() {
+                     Configuration.assertionMode = SOFT;
+                     open("page.html");
+                
+                     $("#first").should(visible).click();
+                     $("#second").should(visible).click();
+                   }
+                 }""";
+
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $(byTagAndText("a", "Soft assertions")).click();
-        $$("div h4").find(text("JUnit5")).shouldBe(visible);
+        $$("div h4").find(text("JUnit5")).parent().sibling(0)
+                .shouldHave(text(expectedText));
+
     }
 }
