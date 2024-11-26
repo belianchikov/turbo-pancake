77feda08306e7370e1427d5e2aef675324cda579
Igor Belianchikov <belyanchikovia@gmail.com>
2024-11-26 22:02:26 +0100

    Added allure attachments.

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import pages.RegistrationForm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,11 +8,14 @@ import utils.TestDataGenerator;
 
 import java.util.Date;
 
+import static io.qameta.allure.Allure.step;
+
 @DisplayName("Student Registration Form tests")
 public class PracticeFormTests extends TestBase {
 
     @DisplayName("Positive test for filling the registration form (maximum)")
     @Test
+    @Tag("simple")
     void positiveMaximumFormFillingTest() {
         Date birthdayDate = TestDataGenerator.getBirthDate();
         String
@@ -31,38 +35,40 @@ public class PracticeFormTests extends TestBase {
                 city = TestDataGenerator.getCity(state);
 
         RegistrationForm registrationForm = new RegistrationForm();
+        step("Open form and fill out", () -> {
+            registrationForm
+                    .openPage()
+                    .removeBanners()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setUserEmail(userEmail)
+                    .setGender(gender)
+                    .setUserNumber(userNumber)
+                    .setDateOfBirth(birthdayDay, birthdayMonth, birthdayYear)
+                    .setSubject(subject)
+                    .setHobby(hobby)
+                    .uploadPicture(pictureName)
+                    .setCurrentAddress(currentAddress)
+                    .selectState(state)
+                    .selectCity(city)
+                    .clickSubmitButton()
+                    .verifyResultsFormAppeared();
+        });
+        step("Verify results", () -> {
+            registrationForm
+                    .verifyFieldValue("Student name", firstName + " " + lastName)
+                    .verifyFieldValue("Student Email", userEmail)
+                    .verifyFieldValue("Gender", gender)
+                    .verifyFieldValue("Mobile", userNumber)
+                    .verifyFieldValue("Date of Birth", birthdayDay + " " + birthdayMonth + "," + birthdayYear)
+                    .verifyFieldValue("Subjects", subject)
+                    .verifyFieldValue("Hobbies", hobby)
+                    .verifyFieldValue("Picture", pictureName)
+                    .verifyFieldValue("Address", currentAddress)
+                    .verifyFieldValue("State and City", state + " " + city);
 
-        registrationForm
-                .openPage()
-                .removeBanners()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(gender)
-                .setUserNumber(userNumber)
-                .setDateOfBirth(birthdayDay, birthdayMonth, birthdayYear)
-                .setSubject(subject)
-                .setHobby(hobby)
-                .uploadPicture(pictureName)
-                .setCurrentAddress(currentAddress)
-                .selectState(state)
-                .selectCity(city)
-                .clickSubmitButton()
-                .verifyResultsFormAppeared();
-
-        registrationForm
-                .verifyFieldValue("Student name", firstName + " " + lastName)
-                .verifyFieldValue("Student Email", userEmail)
-                .verifyFieldValue("Gender", gender)
-                .verifyFieldValue("Mobile", userNumber)
-                .verifyFieldValue("Date of Birth", birthdayDay + " " + birthdayMonth + "," + birthdayYear)
-                .verifyFieldValue("Subjects", subject)
-                .verifyFieldValue("Hobbies", hobby)
-                .verifyFieldValue("Picture", pictureName)
-                .verifyFieldValue("Address", currentAddress)
-                .verifyFieldValue("State and City", state + " " + city);
-
-        registrationForm.closeResultsForm();
+            registrationForm.closeResultsForm();
+        });
     }
 
     @DisplayName("Positive test for filling the registration form (minimum)")
